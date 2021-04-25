40cced52841bd8a6d2001c185968f74cdbea01b0
Bhavy <bhavyghetiya9@gmail.com>
2021-04-25 12:52:01 +0530

    25/4/21

--- app/src/main/java/com/example/restaurant_app/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/restaurant_app/Retrofit/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static String BASE_URL = "http://192.168.0.106:8080";
+    private static String BASE_URL = "http://192.168.0.104:8080";
     private static Retrofit retrofit = null;
 
     public static Retrofit getInstance() {
